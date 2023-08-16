c8416cb5ab0efc33c54e9ad2fed95f92348c51fe
Artyom Narshinov <narshinovartyom@mail.ru>
2023-08-16 13:57:19 +0400

    chat full ui, send messages to state

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import Chat from "./components/Chat/Chat";
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <Chat />
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="title">
+				<h1>Bot Chat</h1>
+				<p>AI-based service</p>
+			</div>
+			<Chat />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import { Input } from "antd";
+import { Form, Input } from "antd";
 
 import bot from "../../assets/bot-icon.svg";
 import me from "../../assets/my-icon.svg";
@@ -9,40 +9,96 @@ import s from "./Chat.module.scss";
 
 interface IChat {}
 
-const Chat: FC<IChat> = () => {
-  const [messages, setMessages] = useState([
-    {
-      id: 0,
-      content: "123",
-      type: "Income",
-    },
-  ]);
+interface IMessage {
+	id: string;
+	content: string;
+	type: string;
+}
 
-  return (
-    <div>
-      <div className={s.content}>
-        {messages.map(({ id, content, type }) => (
-          <div className={s.item} key={id}>
-            <img src={bot} />
-            <div key={id} className={s.message}>
-              {content}
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className={s.form}>
-        <Input
-          className={s.input}
-          placeholder="Start typing here..."
-          suffix={
-            <button className={s.submit}>
-              <img src={buttonIcon} alt="" />
-            </button>
-          }
-        />
-      </div>
-    </div>
-  );
+const Chat: FC<IChat> = () => {
+	const [form] = Form.useForm();
+	const [messages, setMessages] = useState<IMessage[]>([
+		{
+			id: "0",
+			content: "123",
+			type: "Income",
+		},
+		{
+			id: "1",
+			content: "123",
+			type: "Sended",
+		},
+	]);
+
+	const onSendMessage = ({ message }) => {
+		form.validateFields();
+
+		const newMessage = {
+			id: crypto.randomUUID(),
+			content: message,
+			type: "Sended",
+		};
+		setMessages((messages) => [...messages, newMessage]);
+
+		form.resetFields();
+	};
+
+	return (
+		<div>
+			<div className={s.content}>
+				{messages.map(({ id, content, type }) => (
+					<div
+						className={
+							type == "Income"
+								? `${s.item}`
+								: `${s.local} ${s.item}`
+						}
+						key={id}
+					>
+						<img src={bot} className={s.image} />
+						<div
+							key={id}
+							className={
+								type == "Income"
+									? `${s.message}`
+									: `${s.sended} ${s.message}`
+							}
+						>
+							{content}
+						</div>
+					</div>
+				))}
+			</div>
+			<div className={s.form}>
+				<Form
+					form={form}
+					initialValues={{ message: "" }}
+					onFinish={onSendMessage}
+				>
+					<Form.Item
+						name="message"
+						rules={[
+							{
+								required: true,
+								min: 1,
+								message: "Input your message",
+							},
+						]}
+					>
+						<Input
+							className={s.input}
+							placeholder="Start typing here..."
+							suffix={
+								<button className={s.submit} type="submit">
+									<img src={buttonIcon} alt="" />
+								</button>
+							}
+						/>
+					</Form.Item>
+				</Form>
+			</div>
+		</div>
+	);
 };
 
 export default Chat;
